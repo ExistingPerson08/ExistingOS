01ef05259f43aefb2e4e1522d7053f24b3d19e27
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-14 14:29:05 +0200

    Fix conflicts

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,12 +4,8 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+rm -f /root/.bash_logout /root/.bash_profile /root/.bashrc
 
-# this installs a package from fedora repos
 dnf5 install -y @workstation-product-environment
 
 systemctl set-default graphical.target
